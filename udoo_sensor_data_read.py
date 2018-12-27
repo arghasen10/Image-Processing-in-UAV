52dba4ec058352248beeddf4d5bf89ef3bf87fb0
arghasen10 <arghasen10@gmail.com>
2018-12-27 13:01:36 +0530

    Sensor data read from Arduino in python and append in csvfile

--- udoo_sensor_data_read.py
+++ udoo_sensor_data_read.py
@@ -1,6 +1,6 @@
 import time
 import serial
-
+import csv
 
 ser = serial.Serial(
   
@@ -14,6 +14,26 @@ ser = serial.Serial(
 counter=0
 
 
+with open('sensor_data.csv', 'w+') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(["Latitude", "Longitude","start_time","end_time","PM1","PM2.5","PM10","NO2","CO2","CO","Humidity","Temperature"])
+    
+    
 while 1:
    x=ser.readline()
-   print x
+   sensor_data = x.split(',')
+   Latitude = sensor_data[0]
+   Longitude = sensor_data[1]
+   start_time = sensor_data[2]
+   end_time = sensor_data[3]
+   PM1 = sensor_data[4]
+   PM2.5 = sensor_data[5]
+   PM10 = sensor_data[6]
+   NO2 = sensor_data[7]
+   CO2 = sensor_data[8]
+   CO = sensor_data[9]
+   Humidity = sensor_data[10]
+   Temperature = sensor_data[11]
+   with open('sensor_data.csv', 'a') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow([Latitude, Longitude,start_time,end_time,PM1,PM2.5,PM10,NO2,CO2,CO,Humidity,Temperature])
