d8ffe9ec4d6655771cc6abd163b975fa99facbf7
arghasen10 <arghasen10@gmail.com>
2019-01-12 01:46:35 +0000

    Sensor data with realtime,date and MQ135 data

--- udoo_sensor_data_read.py
+++ udoo_sensor_data_read.py
@@ -1,39 +1,34 @@
 import time
 import serial
 import csv
+import datetime
 
-ser = serial.Serial(
-  
-   port='/dev/ttyUSB0',
-   baudrate = 9600,
-   parity=serial.PARITY_NONE,
-   stopbits=serial.STOPBITS_ONE,
-   bytesize=serial.EIGHTBITS,
-   timeout=1
-)
+currenttime = datetime.datetime.now()
+print(str(currenttime))
+print('Started Receiving from Arduino.....')
+
+ser = serial.Serial('/dev/ttymxc3',115200,timeout=1)
+ser.flushOutput()
 counter=0
 
 
-with open('sensor_data.csv', 'w+') as csv_file:
+with open('satya_sensor_data.csv', 'w+') as csv_file:
     csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(["Latitude", "Longitude","start_time","end_time","PM1","PM2.5","PM10","NO2","CO2","CO","Humidity","Temperature"])
+    csv_writer.writerow(["Date","Time","CO2"])
     
     
-while 1:
+while True:
    x=ser.readline()
-   sensor_data = x.split(',')
-   Latitude = sensor_data[0]
-   Longitude = sensor_data[1]
-   start_time = sensor_data[2]
-   end_time = sensor_data[3]
-   PM1 = sensor_data[4]
-   PM2.5 = sensor_data[5]
-   PM10 = sensor_data[6]
-   NO2 = sensor_data[7]
-   CO2 = sensor_data[8]
-   CO = sensor_data[9]
-   Humidity = sensor_data[10]
-   Temperature = sensor_data[11]
-   with open('sensor_data.csv', 'a') as csv_file:
+
+   if x is  '':
+      continue
+   currenttime = datetime.datetime.now()
+   currenttime = str(currenttime)
+   currenttime_array = currenttime.split(' ')
+   date = currenttime_array[0]
+   time = currenttime_array[1][:8]
+   x = x.strip()
+   print(date,time,x)
+   with open('satya_sensor_data.csv', 'a') as csv_file:
             csv_writer = csv.writer(csv_file)
-            csv_writer.writerow([Latitude, Longitude,start_time,end_time,PM1,PM2.5,PM10,NO2,CO2,CO,Humidity,Temperature])
+            csv_writer.writerow([date, time,x])
