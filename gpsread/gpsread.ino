dd95fcdc01da18d304d952aa10079404aeb5db05
ARGHA SEN <arghasen10@gmail.com>
2019-01-13 04:38:09 +0530

    GPS working Code

--- gpsread/gpsread.ino
+++ gpsread/gpsread.ino
@@ -1,45 +1,26 @@
-#include <LiquidCrystal.h> 
-#include <SoftwareSerial.h> 
-#include <TinyGPS.h> 
-float lat = 28.5458,lon = 77.1703; // create variable for latitude and longitude object  
-SoftwareSerial gpsSerial(3,4);//rx,tx 
-LiquidCrystal lcd(A0,A1,A2,A3,A4,A5); 
-TinyGPS gps; // create gps object 
-void setup(){ 
-Serial.begin(9600); // connect serial 
-//Serial.println("The GPS Received Signal:"); 
-gpsSerial.begin(9600); // connect gps sensor 
-lcd.begin(16,2); 
-} 
-void loop(){ 
-  while(gpsSerial.available()){ // check for gps data 
-  if(gps.encode(gpsSerial.read()))// encode gps data 
-  {  
-  gps.f_get_position(&lat,&lon); // get latitude and longitude 
-  // display position 
-  lcd.clear(); 
-  lcd.setCursor(1,0); 
-  lcd.print("GPS Signal"); 
-  //Serial.print("Position: "); 
-  //Serial.print("Latitude:"); 
-  //Serial.print(lat,6); 
-  //Serial.print(";"); 
-  //Serial.print("Longitude:"); 
-  //Serial.println(lon,6);  
-  lcd.setCursor(1,0); 
-  lcd.print("LAT:"); 
-  lcd.setCursor(5,0); 
-  lcd.print(lat); 
-  //Serial.print(lat); 
-  //Serial.print(" "); 
-  lcd.setCursor(0,1); 
-  lcd.print(",LON:"); 
-  lcd.setCursor(5,1); 
-  lcd.print(lon); 
- } 
-} 
-String latitude = String(lat,6); 
-  String longitude = String(lon,6); 
-Serial.println(latitude+";"+longitude); 
-delay(1000); 
-} 
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
+TinyGPSPlus gps;
+
+SoftwareSerial mySerial(8,9);
+
+void setup() {
+  mySerial.begin(9600);
+  Serial.begin(9600);
+  Serial.println("GPS start");
+}
+
+void loop() {
+  while (mySerial.available()) {
+    gps.encode(mySerial.read());
+  }
+  if (gps.location.isUpdated()) {
+    //double a = gps.location.lat();
+    //Serial.println(a);
+    Serial.print(gps.location.lat(), 6);
+    //double b = gps.location.lng();
+    Serial.print(" , ");
+    Serial.print(gps.location.lng(), 6);
+    Serial.println(" , ");
+  }
+}
