af34f432cc124e4fc553f01c35f369ead39897c4
arghasen10 <arghasen10@gmail.com>
2019-01-13 03:04:17 +0000

    Date+time+Dust+CO2+Humidity+temp

--- udoo_sensor_data_read.py
+++ udoo_sensor_data_read.py
@@ -7,20 +7,23 @@ currenttime = datetime.datetime.now()
 print(str(currenttime))
 print('Started Receiving from Arduino.....')
 
-ser = serial.Serial('/dev/ttymxc3',115200,timeout=1)
+ser = serial.Serial('/dev/ttyMCC',115200,timeout=1)
 ser.flushOutput()
 counter=0
 
 currenttime = datetime.datetime.now()
 currenttime = str(currenttime)
 currenttime_array = currenttime.split(' ')
+date = currenttime_array[0]
 time = currenttime_array[1][:8]
 filename = time
+filename+= date 
 filename+='.csv'
+print('Name of File created :',filename)
 
 with open(filename, 'w+') as csv_file:
     csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(["Date","Time","PM1","PM2.5","PM10","CO2"])
+    csv_writer.writerow(["Date","Time","PM1","PM2.5","PM10","CO2","Humidity","Temperature"])
     
     
 while True:
@@ -38,8 +41,10 @@ while True:
    PM1 = int(x[0])
    PM2_5 = int(x[1])
    PM10 = int(x[2])
-   CO2 = int(x[-1])
-   print(date,time,PM1,PM2_5,PM10,CO2)
+   CO2 = int(x[3])
+   humi = float(x[4])
+   temp = float(x[5])
+   print(date,time,PM1,PM2_5,PM10,CO2,humi,temp)
    with open(filename, 'a') as csv_file:
             csv_writer = csv.writer(csv_file)
-            csv_writer.writerow([date,time,PM1,PM2_5,PM10,CO2])
+            csv_writer.writerow([date,time,PM1,PM2_5,PM10,CO2,humi,temp])
