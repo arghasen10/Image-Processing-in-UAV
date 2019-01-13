282d0ecc80f98c8efe19838085f521ded66a33d0
arghasen10 <arghasen10@gmail.com>
2019-01-13 14:55:15 +0000

    Receiving GPS as well as Sensor data with ttyACM0 and ttyMCC

--- serial-transfer.py
+++ serial-transfer.py
@@ -2,15 +2,23 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/ttymxc3',115200,timeout=1)
+ser = serial.Serial('/dev/ttyACM0',115200,timeout=1)
 ser.flushOutput()
 
+ser2 = serial.Serial('/dev/ttyMCC',115200,timeout = 1)
+ser.flushOutput()
+ 
 while True:
    x=ser.readline()
+   y = ser2.readline()
 
    if x is  '':
       continue
+   if y is '':
+      continue
+
    x = x.strip()
-   print(x)
-   x = x.split(',')
-   print('PM1 = ',x[0],'PM2.5 = ',x[1],'PM10 = ',x[2],'CO2 = ',x[-1])
+   y = y.strip()
+   print(x,y)
+   #x = x.split(',')
+   #print('PM1 = ',x[0],'PM2.5 = ',x[1],'PM10 = ',x[2],'CO2 = ',x[-1])
