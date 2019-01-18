431071f660ab1a7b8ffc14faa5f91df61b68f89a
arghasen10 <arghasen10@gmail.com>
2019-01-18 02:17:40 +0000

    Changes implemented for multichannel in the main code

--- aruino-data/aruino-data.ino
+++ aruino-data/aruino-data.ino
@@ -1,21 +1,15 @@
-//#include <MutichannelGasSensor.h>
+
 
 #include <TH02_dev.h>
 
 #include <SoftwareI2C.h>
 #include <Wire.h>
-//#include <SoftwareSerial.h>
-//#include <TinyGPS++.h>
+#include <MutichannelGasSensor.h>
 #define LENG 31   //0x42 + 31 bytes equal to 32 bytes
 unsigned char buf[LENG];
 int PM01Value=0;          //define PM1.0 value of the air detector module
 int PM2_5Value=0;         //define PM2.5 value of the air detector module
-int PM10Value=0;         //define PM10 value of the air detector module
-//String dataString = "";
-////SoftwareSerial Serial0(12,13); // RX, TX
-//SoftwareSerial gpsSerial(10,11); //RX,TX
-//TinyGPSPlus gps;
-//double lattitude,longitude;
+int PM10Value=0;   
 int sensorValue;
 int digitalValue;
 #define GAS_EN      1
@@ -24,13 +18,12 @@ const int pin_scl = 2;      // select a pin as SCL of software I2C
 const int pin_sda = 3;      // select a pin as SDA of software I2C
 int flag=0;
 int writeflag =0;
-////Latitude,Longitude,Time,Date,PM1,PM2_5,PM10,N02,CO2,CO,Humidity,Temperature
 String lati,lon,tim,dat,pm1val,pm2_5val,pm10_val,no2_val,co2_val,co_val,humid_val,temper_val;
 void setup()
 {
   Serial.begin(9600);
   setuphumi();
-//  setupmulti();
+ setupmulti();
   setupdust();
   setuphumi();
 }
@@ -38,9 +31,9 @@ void setup()
 void loop()
 {
   readdust();
- // readmultino2();
+ readmultino2();
   readMQ135();
- // readmultico();
+  readmultico();
   readhumi();
   delay(1000);
 
@@ -129,29 +122,29 @@ int transmitPM10(unsigned char *thebuf)
   return PM10Val;
 }
 
-//void setupmulti()
-//{
-//    gas.begin(0x04);//the default I2C address of the slave is 0x04
-//
-//}
-//
-//void readmultino2()
-//{ 
-//    float n;  
-//    n = gas.measure_NO2();    
-//    Serial.print(n);
-//    Serial.print(",");
-//}
-//
-//
-//void readmultico()
-//{ 
-//    float c;
-//    c = gas.measure_CO();
-//    Serial.print(c);
-//    Serial.print(",");
-//}
-//////////////////////////////////////////////////////////////
+void setupmulti()
+{
+    gas.begin(0x04);//the default I2C address of the slave is 0x04
+
+}
+
+void readmultino2()
+{ 
+    float n;  
+    n = gas.measure_NO2();    
+    Serial.print(n);
+    Serial.print(",");
+}
+
+
+void readmultico()
+{ 
+    float c;
+    c = gas.measure_CO();
+    Serial.print(c);
+    Serial.print(",");
+}
+////////////////////////////////////////////////////////////
 
 void setuphumi()
 {  
