0336080c3ba19da788f4d44230f5efdcd63a76ab
ARGHA SEN <arghasen10@gmail.com>
2019-05-24 18:55:43 +0530

    Updated necessary changes

--- udoo_sensor_data_read.py
+++ udoo_sensor_data_read.py
@@ -1,80 +1,79 @@
-import time as Tmodule
+import time as tm
 import serial
-import csv,os
+import csv
 import datetime
+import drone_server as ds
 
 currenttime = datetime.datetime.now()
-#print(str(currenttime))
-#print('Started Receiving from Arduino.....')
+print(str(currenttime))
+print('Started Receiving from Arduino.....')
 
 ser = serial.Serial('/dev/ttyMCC',115200,timeout=1)
 ser.flushOutput()
-counter=0
+
+ds.socket_create()
+ds.socket_bind()
+conn = ds.accept_connections()
 
 currenttime = datetime.datetime.now()
 currenttime = str(currenttime)
 currenttime_array = currenttime.split(' ')
 date = currenttime_array[0]
 time = currenttime_array[1][:8]
-filename = time
-filename+='_'
-filename+= date
+mode = time.split(':')
+s1 = int(mode[0])
+s2 = int(mode[1])
+s3 = int(mode[2])
+s1 = s1*10000+s2*100+s3
+time = s1
+mode = date.split('-')
+s1 = int(mode[0])
+s2 = int(mode[1])
+s3 = int(mode[2])
+s1 = s1*10000+s2*100+s3
+date = s1
+filename = 'output'+str(time)+'_'+str(date)
 filename+='.csv'
-
-print('[INFO] Name of File created : '+filename)
-
-filename = os.path.join('/home/udooer/Desktop/aetdrone/Image-Processing-in-UAV/new_output',filename)
-
+print('Name of File created :',filename)
+header_name = ["Date","Time","PM1","PM2.5","PM10","NO2","CO2","CO","Humidity","Temperature"]
 with open(filename, 'w+') as csv_file:
     csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(["Date","Time","PM1","PM2.5","PM10","NO2","CO2","CO","Humidity","Temperature"])
+    csv_writer.writerow(header_name)
 
-#file1 = open("/gpio/pin25/value","r")
-#t_old = file1.read()
-#t_old = int(t_old)
 
+start_time = tm.time()
 while True:
    x=ser.readline()
-
-   if x is  '':
-      continue
-
-currenttime = datetime.datetime.now()
+   #print(x)
+   end_time = tm.time()
+   if x is  None:
+            continue
+   now_time = end_time-start_time
+   if now_time<5:
+            print(x)
+            print("Ignore state")
+            continue
+   currenttime = datetime.datetime.now()
    currenttime = str(currenttime)
    currenttime_array = currenttime.split(' ')
    date = currenttime_array[0]
    time = currenttime_array[1][:8]
-   x = x.strip().split(',')
-   if len(x)<8:
-        continue
-
-   PM1,PM2_5,PM10,NO2,CO2,CO,humi,temp=map(float,x)
-
-   #file1 = open("/gpio/pin25/value","r")
-   #t = file1.read()
-   #t = int(t)
-   #file3 = open("/gpio/pin21/value","w")
-   #if(t==1):
-#       file3.write("0\n")
- #  elif(t==0):
-#       file3.write("1\n")
-
- #  if(t!=t_old):
-#       print("[INFO] Stopped by hardware Interrupt")
-#       break
-
-   #file = open("/gpio/pin22/value","w")
-   #file.write("1\n")
-   #file.close()
-   Tmodule.sleep(0.1)
-   print('[INFO] Date '+date+'  ,Time '+time+'  ,PM1 '+str(PM1)+'  ,PM2_5 '+str(PM2_5)+'  ,PM10 '+str(PM10)+'  ,NO2 '+str(NO2)+'  ,CO2 '+str(CO2)+'  ,CO '+str(CO)+'  ,$
+   x = x.strip()
+   x = str(x,'utf-8')
+   x = x.split(',')
+   if len(x) !=8:
+            continue
+   PM1 = int(x[0])
+   PM2_5 = int(x[1])
+   PM10 = int(x[2])
+   NO2 = float(x[3])
+   CO2 = int(x[4])
+   CO = float(x[5])
+   humi = float(x[6])
+   temp = float(x[7])
+   print(date,time,PM1,PM2_5,PM10,NO2,CO2,CO,humi,temp)
+   data_list = [date,time,PM1,PM2_5,PM10,NO2,CO2,CO,humi,temp]
    with open(filename, 'a') as csv_file:
             csv_writer = csv.writer(csv_file)
-            csv_writer.writerow([date,time,PM1,PM2_5,PM10,NO2,CO2,CO,humi,temp])
-  # file = open("/gpio/pin22/value","w")
-  # file.write("0\n")
-   #file.close()
-   #file1.close()
-  # file3.close()
-
-
+            csv_writer.writerow(data_list)
+   data_str=','.join(str(e) for e in data_list)
